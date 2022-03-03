34008c97883c919a5412b7dfb5b8601414947411
nathan <nathan@olightworld.com>
2022-03-03 16:36:58 +0800

    '优化路由鉴权函数'

--- src/common/auth.router.js
+++ src/common/auth.router.js
@@ -1,4 +1,3 @@
-
 /*
 * 路由鉴权高阶组件
 */
@@ -7,17 +6,18 @@ import { Navigate, useLocation } from 'react-router-dom'
 import store from '@/redux/store'
 import * as Actions from '@/redux/actions'
 
+const Routers = ({ children }) => {
+    let auth = store.getState().authorization || '' // 是否登录
+    let location = useLocation()
+    store.dispatch(Actions.currentIndex(location.pathname))// 记录当前激活栏
+    
+    if (!auth) {
+      return <Navigate to="/login" state={{ from: location }} replace />
+    }
+    if(auth && location.pathname === "/login") {
+      return <Navigate to="/" state={{ from: location }} replace />
+    }
+    return children
+  }
 
-export default ({ children }) => {
-  let auth = store.getState().authorization || '' // 是否登录
-  let location = useLocation()
-  store.dispatch(Actions.currentIndex(location.pathname))// 记录当前激活栏
-  
-  if (!auth) {
-    return <Navigate to="/login" state={{ from: location }} replace />
-  }
-  if(auth && location.pathname == "/login") {
-    return <Navigate to="/" state={{ from: location }} replace />
-  }
-  return children
-}
+export default Routers
