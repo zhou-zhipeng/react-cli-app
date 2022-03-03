2a18e729907945e3295350d10b14b7b00001d5fe
nathan <nathan@olightworld.com>
2022-03-03 16:39:48 +0800

    '新增客户管理深数据联调'

--- src/common/regexp.js
+++ src/common/regexp.js
@@ -1,4 +1,5 @@
 // 公共的正则匹配规则
 export const regExp = {
   accountNo: /^[0-9A-Za-z]{4,10}$/, // 账号4-10位数字或字母组成
+  desPhoneNum: /(\d{3})\d*(\d{4})/
 }
--- src/page/customer/index.js
+++ src/page/customer/index.js
@@ -1,25 +1,119 @@
 // 未找到页面组件
 import '@/less/module/customer.less'
-import { Component } from 'react';
+import { Component } from 'react'
 
-// import { Progress, Button } from 'antd'
+// api
+import { apiGetUserList } from '@/api/customer'
+import { regExp } from '@/common/regexp'
+import { Table, Tag, Space } from 'antd'
 export default class Customer extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
+  constructor(props) {
+    super(props)
+    this.state = {
+      columns: [
+        {
+          title: '姓名',
+          dataIndex: 'name',
+          key: 'name',
+          align: 'left',
+          render: (text) => <span className='btn-span'>{text}</span>,
+        },
+        {
+          title: '年龄',
+          dataIndex: 'age',
+          key: 'age',
+          align: 'center',
+        },
+        {
+          title: '住宅',
+          dataIndex: 'address',
+          key: 'address',
+          align: 'center',
+        },
+        {
+          title: '等级',
+          dataIndex: 'level',
+          key: 'level',
+          align: 'center',
+          render: (level) => (
+            <>
+              {
+                <Tag color={this.getLevel(level).color} key={level}>
+                  {this.getLevel(level).value}
+                </Tag>
+              }
+            </>
+          ),
+        },
+        {
+          title: '电话',
+          dataIndex: 'phone',
+          key: 'phone',
+          align: 'center',
+          render: (phone) => phone.replace(regExp.desPhoneNum, '$1****$2'),
+        },
+        {
+          title: '邮箱',
+          dataIndex: 'e_mail',
+          key: 'e_mail',
+          align: 'center',
+        },
+        {
+          title: '操作',
+          key: 'action',
+          align: 'center',
+          render: (text, record) => (
+            <Space size="middle">
+              <span className='btn-span'>修改</span>
+              <span className='btn-span del'>删除</span>
+            </Space>
+          ),
+        },
+      ],
+      dataSource: [],
+    }
+  }
 
+  UNSAFE_componentWillMount() {
+    this.getList()
+  }
+
+  getLevel(level) {
+    switch (level) {
+      case 1:
+        return {color:'magenta', value: '红钻'}
+      case 2:
+        return {color:'purple', value: '紫钻'}
+      case 3:
+        return {color:'cyan', value: '蓝钻'}
+      case 4:
+        return {color:'gold', value: '黄金'}
+      case 5:
+        return {color:'volcano', value: '黄铜'}
+      default:
+        return {color:'volcano', value: '黄铜'}
+    }
+  }
+
+  getList() {
+    apiGetUserList().then((res) => {
+        let { data } = res
+        if (data.status === 200) {
+          this.setState({
+            dataSource: data.data,
+          })
         }
-    }
-    gobacks() {
+      }).catch((err) => {
+        console.log(err)
+      })
+  }
 
-    }
-    render() {
-        return(
-            <div className="customer-page">
-                <div>
-                   客户
-                </div>
-            </div>
-        )
-    }
+  render() {
+    const { columns, dataSource } = this.state
+    return (
+      <div className="customer-page">
+        <Table columns={columns} dataSource={dataSource} />
+      </div>
+    )
+  }
 }
