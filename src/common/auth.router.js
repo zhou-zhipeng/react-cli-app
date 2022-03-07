612a583decc8d21bddfd5a4d27fe65ec2aa4da72
nathan <nathan@olightworld.com>
2022-03-07 13:14:25 +0800

    '优化记录激活菜单选项'

--- src/common/auth.router.js
+++ src/common/auth.router.js
@@ -4,13 +4,10 @@
 import { Navigate, useLocation } from 'react-router-dom'
 //引入store和action
 import store from '@/redux/store'
-import * as Actions from '@/redux/actions'
 
 const Routers = ({ children }) => {
     let auth = store.getState().authorization || '' // 是否登录
     let location = useLocation()
-    store.dispatch(Actions.currentIndex(location.pathname))// 记录当前激活栏
-    
     if (!auth) {
       return <Navigate to="/login" state={{ from: location }} replace />
     }
