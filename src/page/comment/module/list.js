0d75b53f71dc09985361388234d2f08a2e818519
nathan <nathan@olightworld.com>
2022-03-07 18:01:41 +0800

    '评论列表mock接口联调'

--- src/page/comment/module/list.js
+++ src/page/comment/module/list.js
@@ -1,27 +1,85 @@
-// 评论页面组件
+// 评论列表组件,使用hooks函数组件
 import '@/less/module/comment.less'
+import React, { useState, useEffect } from 'react'
+import { Table, Tooltip , Space, message } from 'antd'
 
-import { Component } from 'react';
+// api
+import { apiGetCommentList } from '@/api/comment'
+export default () => {
+  const [loading, setLoading] = useState(false)
+  const [tableList, setTableList] = useState([])
+  const [columns, setColumns] = useState([
+    {
+      title: '用户名',
+      dataIndex: 'name',
+      key: 'name',
+      align: 'left',
+      render: (text) => <a>{text}</a>,
+    },
+    {
+      title: '标题',
+      dataIndex: 'title',
+      align: 'center',
+      key: 'age',
+    },
+    {
+      title: '内容简介',
+      dataIndex: 'contents',
+      align: 'center',
+      key: 'address',
+      ellipsis: {
+        showTitle: false,
+      },
+      render: contents => (
+        <Tooltip placement="topLeft" title={contents}>
+          {contents}
+        </Tooltip>
+      ),
+    },
+    {
+      title: '操作',
+      key: 'action',
+      align: 'center',
+      render: (text, record, index) => (
+        <Space size="middle">
+          <a onClick={() => passList(index)}>通过</a>
+          <a onClick={() => disList(index)}>禁用</a>
+          <a onClick={() => replyList(index)}>回复</a>
+        </Space>
+      ),
+    },
+  ])
 
+  const passList = (index) => {
+    message.success('操作成功');
+    console.log(`通过评论! -- ${index}`)
+  }
 
-// import { Progress, Button } from 'antd'
-export default class Comment extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
+  const disList = (index) => {
+    message.success('禁用成功');
+    console.log(`禁用成功! -- ${index}`)
+  }
 
-        }
-    }
-    gobacks() {
-;
-    }
-    render() {
-        return(
-            <div className="comment-page">
-                <div>
-                    评论列表
-                </div>
-            </div>
-        )
-    }
+  const replyList = (index) => {
+    message.success('回复成功');
+    console.log(`回复成功! -- ${index}`)
+  }
+
+  const getList = async param => {
+    setLoading(true)
+    setTableList([])
+    const { data } = await apiGetCommentList(param)
+    setTableList(data.data)
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    getList()
+  }, [])
+
+  return (
+    <div className="comment-page">
+      <Table columns={columns} dataSource={tableList} loading={loading}/>
+    </div>
+  )
 }
